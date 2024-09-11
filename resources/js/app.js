c19b6b0a4bfd6327adfabe49d5d28aaab21a643e
Dgbusiness <dgbusiness26@gmail.com>
2024-09-11 10:19:15 -0400

    Vue and Tailwind installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import Sample from './components/Sample.vue'
+
+
+const app = createApp({
+    components: {
+        Sample,
+    }
+})
+
+
+app.mount("#app")
